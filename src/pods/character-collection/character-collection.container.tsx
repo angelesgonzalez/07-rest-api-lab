cbcb88ad28d73fb3c576b0ae82cbb43c9d84dd67
Angeles <delosangelesgonzalez@gmail.com>
2026-09-04 13:28:44 +0200

    Refactor: character-collection-container to recieve page, setPages, totalPags and use it for pagination

--- src/pods/character-collection/character-collection.container.tsx
+++ src/pods/character-collection/character-collection.container.tsx
@@ -5,13 +5,13 @@ import { useCharacterCollection } from './character-collection.hook';
 import { CharacterCollectionComponent } from './character-collection.component';
 
 export const CharacterCollectionContainer = () => {
-  const { characterCollection, loadCharacterCollection } =
+  const { characterCollection, loadCharacterCollection, page, setPage, totalPages } =
     useCharacterCollection();
   const navigate = useNavigate();
 
   React.useEffect(() => {
     loadCharacterCollection();
-  }, []);
+  }, [page]);
 
   const handleCreateCharacter = () => {
     navigate(linkRoutes.createCharacter);
@@ -21,12 +21,22 @@ export const CharacterCollectionContainer = () => {
     navigate(linkRoutes.editCharacter(id));
   };
 
+  const handleNextPage = () => {
+    setPage((currentPage: number) => currentPage + 1)
+  }
+  const handlePrevPage = () => {
+    setPage((currentPage: number) => currentPage - 1)
+  }
 
   return (
     <CharacterCollectionComponent
       characterCollection={characterCollection}
       onCreateCharacter={handleCreateCharacter}
       onEdit={handleEdit}
+      page={page}
+      totalPages={totalPages}
+      onNextPage={handleNextPage}
+      onPrevPage={handlePrevPage}
     />
   );
 };
